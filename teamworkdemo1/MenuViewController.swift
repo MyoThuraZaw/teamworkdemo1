03d20eae93a34c084c3f7c55cf3f05cd28feb9b8
devthet <devthet@gmail.com>
2019-09-01 10:32:45 +0630

    added menu storyboard

---
+++ teamworkdemo1/MenuViewController.swift
@@ -0,0 +1,30 @@
+//
+//  MenuViewController.swift
+//  teamworkdemo1
+//
+//  Created by Thet Tun Oo on 9/1/19.
+//  Copyright © 2019 Min Aung Hein. All rights reserved.
+//
+
+import UIKit
+
+class MenuViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
