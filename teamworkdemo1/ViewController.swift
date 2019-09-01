7b23a130c90223b51acc1f3e90e5b5c2bc4b32c9
Zara Min <zaraminn@gmail.com>
2019-09-01 10:57:34 +0630

    fix Login

--- teamworkdemo1/ViewController.swift
+++ teamworkdemo1/ViewController.swift
@@ -14,16 +14,16 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         
-        let menuStoryboard = UIStoryboard(name: "Menu", bundle: nil)
-        let vc = menuStoryboard.instantiateInitialViewController() as! MenuViewController
+//        let menuStoryboard = UIStoryboard(name: "Menu", bundle: nil)
+//        let vc = menuStoryboard.instantiateInitialViewController() as! MenuViewController
         
         let storyBoard = UIStoryboard(name: "Loading", bundle: nil)
         let vc = storyBoard.instantiateInitialViewController() as! LoadingViewController
         present(vc, animated: true, completion: nil)
-
-        let storyBoard = UIStoryboard(name: "HomeStoryboard", bundle: nil)
-        let vc = storyBoard.instantiateInitialViewController() as! HomeViewController
-        present(vc,animated:true,completion: nil)
+//
+//        let storyBoard = UIStoryboard(name: "HomeStoryboard", bundle: nil)
+//        let vc = storyBoard.instantiateInitialViewController() as! HomeViewController
+//        present(vc,animated:true,completion: nil)
     }
 
 
