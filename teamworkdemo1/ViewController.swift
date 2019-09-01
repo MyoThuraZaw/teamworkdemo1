4f58aa1f7f17c6fc713bbeaf52779c666ecc4010
Unknown <minaunghein@gmail.com>
2019-09-01 11:02:21 +0630

    fix loading

--- teamworkdemo1/ViewController.swift
+++ teamworkdemo1/ViewController.swift
@@ -14,16 +14,22 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         
-        let menuStoryboard = UIStoryboard(name: "Menu", bundle: nil)
-        let vc = menuStoryboard.instantiateInitialViewController() as! MenuViewController
+//        let menuStoryboard = UIStoryboard(name: "Menu", bundle: nil)
+//        let vc = menuStoryboard.instantiateInitialViewController() as! MenuViewController
+//
+      
+//
+//        let storyBoard = UIStoryboard(name: "HomeStoryboard", bundle: nil)
+//        let vc = storyBoard.instantiateInitialViewController() as! HomeViewController
+//        present(vc,animated:true,completion: nil)
         
+        
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
         let storyBoard = UIStoryboard(name: "Loading", bundle: nil)
         let vc = storyBoard.instantiateInitialViewController() as! LoadingViewController
         present(vc, animated: true, completion: nil)
-
-        let storyBoard = UIStoryboard(name: "HomeStoryboard", bundle: nil)
-        let vc = storyBoard.instantiateInitialViewController() as! HomeViewController
-        present(vc,animated:true,completion: nil)
     }
 
 
